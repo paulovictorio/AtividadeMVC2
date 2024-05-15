b04c63bb5ba9ae868fd77a67f700b050c665ec2e
paulovictorio <paulocesarvictorio940@gmail.com>
2024-05-15 20:09:58 -0300

    Primeiro Commit

--- app.js
+++ app.js
@@ -11,19 +11,18 @@ const auth = require('./auth');
 
 // Configurar acesso à BD.
 const mongoose = require('mongoose');
-//let url = 'mongodb+srv://janaina:12346@fatec.r3iqzia.mongodb.net/?retryWrites=true&w=majority&appName=Fatec';
 const url = 'mongodb://127.0.0.1:27017/fatec';
 const mongoDB = process.env.MONGODB_URI || url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
-let db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro ao conectar ao MongoDB'));
-
-app.use(loginRoute);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(loginRoute);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
 // Middleware de autenticação
@@ -45,8 +44,8 @@ function authenticateToken(req, res, next) {
 }
 
 app.use(authenticateToken);
-app.use(userRoute)
-app.use(projectRoute)
+app.use(userRoute);
+app.use(projectRoute);
 
   app.listen(3000, () => {
     console.log('Servidor em execução na porta 3000');
